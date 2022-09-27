3b14e9202d5cc82cba21fc19b1a1557b7b994abb
407 <407@academicos.cesde.edu.co>
2022-09-27 17:35:29 -0500

    se crea app de react

---
+++ src/Componente/Angel.js
@@ -0,0 +1,12 @@
+export function Angel(){
+    let nombre="Angel Soto"
+    //interpolar
+    return(
+        <>
+            <h1>HOLA REACT SOY {nombre} </h1>
+            <hr></hr>
+            
+        </>
+       
+    )
+}
---
+++ src/Sinopsis/Sinopsis.js
@@ -0,0 +1,11 @@
+import "./Sinopsis.css"
+export function Sinopsis(){
+    return(
+        <>
+            <div class="sinopsis">
+                <h1>Sinopsis</h1>
+                <p>lorem ipsum lo que sea porque no se autocompleta</p>
+            </div>
+        </>
+    )
+}
